6ef5b4583e7bafa64f3f52962bd4cf1d0990ea8a
Tran Tien Anh <tienanh3tran@gmail.com>
2023-07-06 00:51:05 +0700

    Update TinyLine() method

--- PhoneStore/Ultilities/ConsoleUlts.cs
+++ PhoneStore/Ultilities/ConsoleUlts.cs
@@ -36,6 +36,10 @@ namespace Ults
 █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████");
         }
 
+        public void TinyLine()
+        {
+            Console.WriteLine("============================================================");
+        }
         public void Title(string? title, string? subTitle)
         {
             if (title != null)
@@ -63,17 +67,20 @@ namespace Ults
             {
                 case Feature.Alert.Success:
                     ConsoleForegroundColor(Enum.UI.Color.Green);
+                    Console.WriteLine("\n" + msg.ToUpper() + "✅");
                     break;
                 case Feature.Alert.Warning:
                     ConsoleForegroundColor(Enum.UI.Color.Yellow);
+                    Console.WriteLine("\n" + msg.ToUpper() + "⚠️");
                     break;
                 case Feature.Alert.Error:
                     ConsoleForegroundColor(Enum.UI.Color.Red);
+                    Console.WriteLine("\n" + msg.ToUpper() + "❌");
                     break;
                 default:
                     break;
             }
-            Console.WriteLine("\n" + msg.ToUpper() + "❌");
+            ;
             ConsoleForegroundColor(Enum.UI.Color.White);
             Ultilities.PressEnterTo("Continue");
         }
@@ -83,23 +90,25 @@ namespace Ults
         }
         public void PrintPhoneDetailsInfo(Phone phone)
         {
-            Console.WriteLine("Phone ID:", phone.PhoneID);
-            Console.WriteLine("Phone name:", phone.PhoneName);
-            Console.WriteLine("Brand:", phone.Brand);
-            Console.WriteLine("CPU:", phone.CPU);
-            Console.WriteLine("RAM:", phone.RAM);
-            Console.WriteLine("Discount Price:", phone.DiscountPrice);
-            Console.WriteLine("Price:", phone.Price);
-            Console.WriteLine("Battery capacity:", phone.BatteryCapacity);
-            Console.WriteLine("OS:", phone.OS);
-            Console.WriteLine("Sim_Slot:", phone.SimSlot);
-            Console.WriteLine("Screen Hz:", phone.ScreenHz);
-            Console.WriteLine("Screen Resolution:", phone.ScreenResolution);
-            Console.WriteLine("ROM:", phone.ROM);
-            Console.WriteLine("Storage Memory:", phone.StorageMemory);
-            Console.WriteLine("Mobile Network:", phone.MobileNetwork);
-            Console.WriteLine("Quantity:", phone.Quantity);
-            Console.WriteLine("Phone Size:", phone.PhoneSize);
+            TinyLine();
+            Console.WriteLine("Phone ID: {0}", phone.PhoneID);
+            Console.WriteLine("Phone Name: {0}", phone.PhoneName);
+            Console.WriteLine("Brand: {0}", phone.Brand);
+            Console.WriteLine("CPU: {0}", phone.CPU);
+            Console.WriteLine("RAM: {0}", phone.RAM);
+            Console.WriteLine("Discount Price: {0}", phone.DiscountPrice);
+            Console.WriteLine("Price: {0}", phone.Price);
+            Console.WriteLine("Battery: {0}", phone.BatteryCapacity);
+            Console.WriteLine("OS: {0}", phone.OS);
+            Console.WriteLine("Sim Slot: {0}", phone.SimSlot);
+            Console.WriteLine("Screen Hz: {0}", phone.ScreenHz);
+            Console.WriteLine("Screen Resolution: {0}", phone.ScreenResolution);
+            Console.WriteLine("ROM: {0}", phone.ROM);
+            Console.WriteLine("Storage Memory: {0}", phone.StorageMemory);
+            Console.WriteLine("Mobile Network: {0}", phone.MobileNetwork);
+            Console.WriteLine("Quantity: {0}", phone.Quantity);
+            Console.WriteLine("Phone Size: {0}", phone.PhoneSize);
+            TinyLine();
         }
     }
 }
